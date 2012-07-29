ebedd688d1cbd94917905e03e0657527f1e48df0
tearose <u.boltyanskaya@gmail.com>
2012-07-29 14:54:39 +0300

    Parce.cs

---
+++ Parce.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PersonNet
+{
+    public class Parce
+    {
+        public static string FindFiledValueXML(string field, string str)
+        {
+            string returnStr = string.Empty;
+            string tag = string.Format("<{0}>(.+?)</{0}>", field);
+            Regex regex = new Regex(tag);
+            Match match = regex.Match(str);
+            if (match.Success)
+            {
+                returnStr = match.Groups[1].Value;
+            }
+            return returnStr;
+        }
+
+    }
+}
